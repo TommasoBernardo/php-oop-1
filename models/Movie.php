c7a37a3f0ee6f5e8c3623d934409fb45ffa21354
TommasoBernardo <tommasob1210@gmail.com>
2023-02-03 17:13:51 +0100

    add methods and construct

--- models/Movie.php
+++ models/Movie.php
@@ -7,4 +7,39 @@ class Movie
     public $cast;
     public $duration;
     public $gender;
+
+    function __construct($_title, $_releaseyear, $_director, $_cast, $_duration, $_gender)
+    {
+        $this->title = $_title;
+        $this->title = $_releaseyear;
+        $this->title = $_director;
+        $this->title = $_cast;
+        $this->title = $_duration;
+        $this->title = $_gender;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function getReleaseYear()
+    {
+        return $this->releaseyear;
+    }
+    public function getDirector()
+    {
+        return $this->director;
+    }
+    public function getCast()
+    {
+        return $this->cast;
+    }
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+    public function getGender()
+    {
+        return $this->gender;
+    }
 }
