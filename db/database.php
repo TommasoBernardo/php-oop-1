e5f13a28913c57b9e1325779160cd73d2688c153
TommasoBernardo <tommasob1210@gmail.com>
2023-02-03 18:19:26 +0100

    update db

--- db/database.php
+++ db/database.php
@@ -8,12 +8,12 @@ $johnWick = new Movie('John Wick - 3', 2019, 'Chad Stahelski', 'Keanu Reeves, Ha
 
 $theHardWay = new Movie('The Hard Way', 2019, ' Keoni Waxman', 'Michael Jai White, Randy Couture, Madalina Anea , Luke Goss', '94 min', 'action');
 
-$coachCarter = new Movie('The Hard Way', 2019, ' Keoni Waxman', 'Michael Jai White, Randy Couture, Madalina Anea , Luke Goss', '94 min', 'action');
+$coachCarter = new Movie('Coach Carter', 2005, 'Thomas Carter', 'Samuel Jackson, Ricky Gonzalez, Rob Brown , Channing Tatum', '136 min', 'dramatic sportive');
 
-$missionImpossible = new Movie('The Hard Way', 2019, ' Keoni Waxman', 'Michael Jai White, Randy Couture, Madalina Anea , Luke Goss', '94 min', 'action');
+$missionImpossible = new Movie('Mission Impossible 4', 2011, 'Brad Bird', 'Tom Cruise, Paula Patton, Simon Pegg , Jeremy Renner', '133 min', 'espionage, action');
 
-$laMummia = new Movie('The Hard Way', 2019, ' Keoni Waxman', 'Michael Jai White, Randy Couture, Madalina Anea , Luke Goss', '94 min', 'action');
+$laMummia = new Movie('La Mummia', 2017, 'Alex Kurtzman', 'Tom Cruise, , Sofia Boutella , Annabelle Wallis, Russell Crowe', '107 min', 'adventure, fantasy, action');
 
-$reArtu = new Movie('The Hard Way', 2019, ' Keoni Waxman', 'Michael Jai White, Randy Couture, Madalina Anea , Luke Goss', '94 min', 'action');
+$reArtu = new Movie('King Arthur - il potere della spada', 2017, ' 	Guy Ritchie', 'Charlie Hunnam, Àstrid Bergès-Frisbey, Djimon Hounsou, Jude Law, Aidan Gillen', '126 min', 'adventure, fantasy, action');
 
 array_push($movies, $johnWick, $theHardWay, $coachCarter, $missionImpossible, $laMummia, $reArtu);
